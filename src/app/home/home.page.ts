6501f3e1b557419db263efe44711e50f98779bb6
fabianFer007 <127862887+fabianFer007@users.noreply.github.com>
2023-09-14 15:17:32 -0300

    creacion de login
    uwu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { IonAvatar,IonModal } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,65 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonAvatar,{read:ElementRef}) avatar!:ElementRef<HTMLIonAvatarElement>;
+  
+  @ViewChild(IonModal) modal!: IonModal;
 
-  constructor() {}
+  private animation!:Animation;
+  constructor(private router: Router,private animationCtrl:AnimationController) { }
+  public mensaje = ""
+  
+  public alertButtons = ['OK'];
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl.create()
+    .addElement(this.avatar.nativeElement)
+    .duration(5000)
+    .iterations(Infinity)
+    .keyframes([
+      {offset:0, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.25, transform:'translateX(100px)',opacity:'0.2'},
+      {offset:0.50, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.75, transform:'translateX(-100px)',opacity:'0.2'},
+      {offset:1, transform:'translateX(0px)',opacity:'1'},
+    ])
+  }
+
+  user = {
+    usuario: "",
+    password: ""
+  }
+  
+  playAvatar(){
+    this.animation.play();
+  }
+
+  enviarInformacion() {
+    if (this.user.usuario != "") {
+      let navigationExtras: NavigationExtras = {
+        state: { user: this.user }
+      }
+      this.router.navigate(['/login'], navigationExtras);
+    } else {
+      this.mensaje = "Debe ingresar sus credenciales";
+    }
+  }
+  mostrarConsola() {
+    console.log(this.user);
+    if (this.user.usuario != "" && this.user.password != "") {
+      this.mensaje = "Usuario Conectado";
+    } else {
+      this.mensaje = "Usuario y contraseña deben tener algun valor"
+    }
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.mensaje="Registro Exitoso"
+    this.modal.dismiss(this.user.usuario, 'confirm');
+  }
 
 }
