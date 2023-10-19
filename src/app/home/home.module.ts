039e58b3f642a6f3389c2fde6bfc8c3e2c058211
fabianFer007 <127862887+fabianFer007@users.noreply.github.com>
2023-10-19 15:44:09 -0300

    un comen
    :)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -14,6 +14,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
+    // Barra de progeso
     MatProgressBarModule
   ],
   declarations: [HomePage]
