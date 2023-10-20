d2c1ceb72a7e23b660395dd072674058da185bb8
fabianFer007 <fabian03fernandez@gmail.com>
2023-10-20 20:28:58 -0300

    servicio local y guard
    UwU

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AutentificarService } from './servicios/autentificar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private auth: AutentificarService) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(this.auth.valido){
+      return true;
+    }else{
+      this.router.navigate(['/home']);
+      return false;
+    }
+
+    
   }
   
 }
--- src/app/servicios/autentificar.service.ts
+++ src/app/servicios/autentificar.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular'
+
+
+interface User {
+  mail: string;
+  pass: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutentificarService {
 
-  constructor() { }
+  public valido!: boolean;
+  private local!: Storage;
+
+  constructor(private storage: Storage, private route: Router) {
+    this.init()
+   }
+
+   async init() {
+    const storage = await this.storage.create();
+    this.local = storage;
+
+  }
+
+  //regitrarse
+  async register(mail: string, pass: string): Promise<Boolean> {
+    const users = await this.local?.get('users') || [];
+    const existe = users.find((us: User) => us.mail === mail && us.pass === pass);
+    if (existe) {
+      console.log('Existente')
+      return true;
+    } else {
+      const nuevo: User = { mail, pass};
+      users.push(nuevo);
+      await this.local.set('users', users);
+      console.log('Registrado')
+      return false;
+    }
+  }
+
+  //logearse
+  async login(mail: string, pass: string): Promise<boolean> {
+    const users: User[] = (await this.local.get('users')) || [];
+    const user = users.find((us: User) => us.mail === mail && us.pass === pass);
+    if (user) {
+      this.valido = true;
+      return true;
+    }
+    this.valido = false;
+    return false;
+  }
+
+  //log out
+  logout() {
+    this.valido = false;
+    this.route.navigate(['/home']);
+  }
+  
 }
